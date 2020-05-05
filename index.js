1102401282d42a12831a9a57d1e5483ae8bad624
Javier Umana <javierumanafs@gmail.com>
2020-05-04 23:00:54 -0400

    mvp

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
 const shortid = require('shortid');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 let userList = [
   {
@@ -50,6 +52,49 @@ server.get('/api/users/:id', (req, res) => {
     res.status(200).json(user)
   } else  if(user === undefined) {
     res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+  } else {
+    res.status(500).json({ errorMessage: "The user information could not be retrieved."})
+  }
+});
+
+server.delete('/api/users/:id', (req, res) =>{
+  const id = req.params.id;
+
+  userList = userList.filter(user => user.id !== id)
+
+  if(userList === undefined){
+    res.status(404).json({ message: "The user with the specified ID does not exist."})
+  } else if(userList){
+    res.status(200).json(userList)
+  } else {
+    res.status(500).json({ errorMessage: "The user could not be removed" })
+  }
+} );
+
+server.patch('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  if(req.body.id !== id){
+    res.status(404).json({ errorMessage: 'The user with the specified ID does not exist.'})
+  } else {
+    res.status(200).json(req.body)
+  }
+  // if(newUserList === undefined){
+  //   res.status(404).json({ errorMessage: 'The user with the specified ID does not exist.'})
+  // } else if(req.body.name && req.body.bio === ''){
+  //   res.status(400).json({ errorMessage: 'Please provide name and bio for the user'})
+  // } else {
+  //   res.status(200).json(newUserList)
+  // }
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  if(req.body.id !== id){
+    res.status(404).json({ errorMessage: 'The user with the specified ID does not exist.'})
+  } else {
+    res.status(200).json(req.body)
   }
 });
 
